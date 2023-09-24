481041185972d9cf6d703690c9d8b980b77ae219
Emily <68402015+emilyyf@users.noreply.github.com>
2023-09-23 21:16:24 -0300

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use rand::prelude::*;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-fn get_rolls() -> [u64; 5] {
-    rand::thread_rng().gen::<[u64; 5]>().map(|a| (a % 6))
-}
-
 fn convert_rolls_to_line(rolls: [u64; 5]) -> u64 {
     let mut a: u64 = 0;
     for (i, roll) in rolls.iter().enumerate() {
@@ -16,17 +12,20 @@ fn convert_rolls_to_line(rolls: [u64; 5]) -> u64 {
 }
 
 fn main() {
+    let mut words: Vec<String> = Vec::new();
     for _ in 0..6 {
-        let rolls = get_rolls();
+        let rolls = rand::thread_rng().gen::<[u64; 5]>().map(|a| (a % 6));
         let line = convert_rolls_to_line(rolls);
 
         let file = File::open("wordlist.txt").expect("Failed to open wordlist file");
         let mut lines = io::BufReader::new(file).lines();
-        let a = lines
+        let word = lines
             .nth(line as usize)
             .expect("Unable to get line")
             .unwrap();
 
-        println!("{}", a);
+        words.push(word);
     }
+
+    println!("{:?}", words)
 }
